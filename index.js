a9713b7f86a46facf55ae85622a9903b1f2406ba
Edward Pearson <epearson@ancestry.com>
2015-06-19 11:13:31 -0700

    added albums and related bands POST options

--- index.js
+++ index.js
@@ -7,83 +7,138 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var makeSlackResponse = function(artist, trackList) {
-  var spotifyLink = "https://open.spotify.com/track/";
-  var textResponse = "Top tracks for "+ artist +"\n";
-  for (var i = 0; i < trackList.length; i++) {
-    var track = trackList[i];
-    textResponse += ('<' + spotifyLink + track.id + '|' + track.name + '>\n');
-  }
-  console.log('Request completed');
-  return {'text' : textResponse};
+var router = express.Router();
+router.use(function (req, res, next) {
+	console.log('Received a request...');
+	next();
+});
+
+// Global vars
+var spotifyUrl = 'https://open.spotify.com/';
+var slackKeywordHook = 'spotify ';
+
+// formats song tracks response for Slack
+var formatSlackTracksResponse = function (artist, trackList) {
+	var spotifyLink = spotifyUrl + 'track/';
+	var textResponse = "Top tracks for " + artist + ":\n";
+
+	for (var i = 0; i < trackList.length; i++) {
+		var track = trackList[i];
+		textResponse += ('<' + spotifyLink + track.id + '|' + track.name + '>\n');
+	}
+
+	return {'text': textResponse};
 };
 
-var router = express.Router();
-router.use(function(req, res, next){
-  console.log('Received a request...');
-  next();
-});
+// formats albums response for Slack
+var formatSlackAlbumsResponse = function (artist, albums) {
+	var spotifyLink = spotifyUrl + 'album/';
+	var textResponse = "Top albums for " + artist + ":\n";
+
+	for (var i = 0; i < albums.length; i++) {
+		var album = albums[i];
+		textResponse += ('<' + spotifyLink + album.id + '|' + album.name + '>\n');
+	}
+
+	return {'text': textResponse};
+};
+
+// formats related artists response for Slack
+var formatSlackArtistsResponse = function (artist, relatedArtists) {
+	var spotifyLink = spotifyUrl + 'artist/';
+	var textResponse = "Related artists for " + artist + ":\n";
+
+	for (var i = 0; i < relatedArtists.length; i++) {
+		var relatedArtist = relatedArtists[i];
+		textResponse += ('<' + spotifyLink + relatedArtist.id + '|' + relatedArtist.name + '>\n');
+	}
+
+	return {'text': textResponse};
+};
 
 // simple hello-world example: http://localhost:8081/api/
-router.get('/', function(req, res) {
-    res.json({text: 'Please enter something like: spotify your_favorite_artist'});
-    res.send('hello!');
+router.get('/', function (req, res) {
+	res.json({text: 'Please enter something like: spotify your_favorite_artist'});
+	res.send('hello!');
 });
 
-// Slack POST example: spotify <artist name>
-router.route('/spotify').post(function(req, res) {
-  var requestString = req.body.text;
-  var spotifyPrefixOffset = ('spotify '.length -1);
-  var artistName = (requestString !== null && requestString !== "" && typeof(requestString) !== "undefined") ? requestString.substring(spotifyPrefixOffset) : "Pearl Jam";
-  console.log('Performing a search for: ' + artistName);
-  spotifySearch.getTracksByArtist(artistName, 5).then(function(trackList) {
-    return res.json(makeSlackResponse(artistName, trackList));
-  }, function(error) {
-    return res.json({"error": error});
-  });
+// Slack POST examples:
+// return top 5 tracks for a given artist: spotify <artist name>
+// return top 5 albums for an artist: spotify <artist name>:albums
+// return top 5 related bands for an artist: spotify <artist name>:related
+router.route('/spotify').post(function (req, res) {
+	var requestString = req.body.text;
+	var spotifyPrefixOffset = (slackKeywordHook.length - 1);
+	var artistName = (requestString !== null && requestString !== "" && typeof(requestString) !== "undefined") ? requestString.substring(spotifyPrefixOffset) : "Pearl Jam";
+
+	if(artistName.lastIndexOf(':albums') > -1) {
+		// get top 5 albums on Spotify for that artist
+		spotifySearch.getArtistAlbums(artistName).then(function (albums) {
+			return res.json(formatSlackAlbumsResponse(artistName, albums));
+		}, function (error) {
+			return res.json({"error": error});
+		});
+	}
+	else if(artistName.lastIndexOf(':related') > -1) {
+		// do search on related artists
+		spotifySearch.getRelatedArtists(artistName).then(function (artists) {
+			return res.json(formatSlackArtistsResponse(artistName, artists));
+		}, function (error) {
+			return res.json({"error": error});
+		});
+	}
+	else {
+		// get top 5 tracks for this artist
+		console.log('Searching top 5 tracks for: ' + artistName);
+		spotifySearch.getTracksByArtist(artistName, 5).then(function (trackList) {
+			return res.json(formatSlackResponse(artistName, trackList));
+		}, function (error) {
+			return res.json({"error": error});
+		});
+	}
 });
 
 // gets artist top tracks by the following example URL: http://localhost:8081/api/spotify/<artist name>
-router.route('/spotify/:artist').get(function(req, res) {
-  var artistName = req.params["artist"];
-  console.log('Performing a search for: ' + artistName);
-  spotifySearch.getTracksByArtist(artistName, 5).then(function(trackList) {
-    return res.json(makeSlackResponse(artistName, trackList));
-  }, function(error) {
-    return res.json({"error": error});
-  });
+router.route('/spotify/:artist').get(function (req, res) {
+	var artistName = req.params["artist"];
+	console.log('Performing a search for: ' + artistName);
+	spotifySearch.getTracksByArtist(artistName, 5).then(function (trackList) {
+		return res.json(formatSlackResponse(artistName, trackList));
+	}, function (error) {
+		return res.json({"error": error});
+	});
 });
 
 // gets artist top tracks by the following example URL: http://localhost:8081/api/spotify?text=<artist name>
-router.route('/spotify').get(function(req, res) {
-  var artistName = req.query.text;
-  console.log('Performing a search for: ' + artistName);
-  spotifySearch.getTracksByArtist(artistName, 5).then(function(trackList) {
-    return res.json(makeSlackResponse(artistName, trackList));
-  }, function(error) {
-    return res.json({"error": error});
-  });
+router.route('/spotify').get(function (req, res) {
+	var artistName = req.query.text;
+	console.log('Performing a search for: ' + artistName);
+	spotifySearch.getTracksByArtist(artistName, 5).then(function (trackList) {
+		return res.json(formatSlackResponse(artistName, trackList));
+	}, function (error) {
+		return res.json({"error": error});
+	});
 });
 
 // gets artist albums by the following example URL: http://localhost:8081/api/spotify/albums/<artist name>
-router.route('/spotify/albums/:artist').get(function(req, res) {
-  var artistName = req.params["artist"];
-  console.log('Performing a search for: ' + artistName);
-  spotifySearch.getArtistAlbums(artistName).then(function(albums) {
-    return res.json(albums);
-  }, function(error) {
-    return res.json({"error": error});
-  });
+router.route('/spotify/albums/:artist').get(function (req, res) {
+	var artistName = req.params["artist"];
+	console.log('Performing a search for: ' + artistName);
+	spotifySearch.getArtistAlbums(artistName).then(function (albums) {
+		return res.json(albums);
+	}, function (error) {
+		return res.json({"error": error});
+	});
 });
 
-router.route('/spotify/related/:artist').get(function(req, res) {
-  var artistName = req.params["artist"];
-  console.log('Performing a search for: ' + artistName);
-  spotifySearch.getRelatedArtists(artistName).then(function(artists) {
-    return res.json(artists);
-  }, function(error) {
-    return res.json({"error": error});
-  });
+router.route('/spotify/related/:artist').get(function (req, res) {
+	var artistName = req.params["artist"];
+	console.log('Performing a search for: ' + artistName);
+	spotifySearch.getRelatedArtists(artistName).then(function (artists) {
+		return res.json(artists);
+	}, function (error) {
+		return res.json({"error": error});
+	});
 });
 
 
--- spotifySearch.js
+++ spotifySearch.js
@@ -1,82 +1,89 @@
 var spotify = require('spotify-web-api-node');
 
-module.exports = new function() {
-  var spotifyClient = new spotify()
-  var spotifyRegion = 'US';
+module.exports = new function () {
+	var spotifyClient = new spotify()
+	var spotifyRegion = 'US';
 
-  // returns a promise to get the searched-on artistId
-  var getArtistID = function(artistName) {
-    var artistsPromise = spotifyClient.searchArtists(artistName);
-    return artistsPromise.then(function(data) 
-    {
-      var artistID = data.artists.items[0].id;
-      return artistID;
-    });
-  };
+	// returns a promise to get the searched-on artistId
+	var getArtistID = function (artistName) {
+		var artistsPromise = spotifyClient.searchArtists(artistName);
+		return artistsPromise.then(function (data) {
+			var artistID = data.artists.items[0].id;
+			return artistID;
+		});
+	};
 
-  this.getTracksByArtist = function(artistName, numberOfTracks) {
-    return getArtistID(artistName).then(function(artistID) {
-      var tracksPromise = spotifyClient.getArtistTopTracks(artistID, spotifyRegion);
-      return tracksPromise.then(function(data) 
-      {
-        var topTracks = [];
-        numberOfTracks = Math.max(0, Math.min(numberOfTracks, data.tracks.length));
+	this.getTracksByArtist = function (artistName, numberOfTracks) {
+		return getArtistID(artistName).then(function (artistID) {
+			var tracksPromise = spotifyClient.getArtistTopTracks(artistID, spotifyRegion);
+			return tracksPromise.then(function (data) {
+				var topTracks = [];
+				numberOfTracks = Math.max(0, Math.min(numberOfTracks, data.tracks.length));
 
-        for (var i = 0; i < numberOfTracks; i++) {
-          topTracks.push({
-            'name': data.tracks[i].name,
-            'id': data.tracks[i].id,
-          });
-        }
+				for (var i = 0; i < numberOfTracks; i++) {
+					topTracks.push({
+						'name': data.tracks[i].name,
+						'id': data.tracks[i].id,
+					});
+				}
 
-        return topTracks;
-      });
-    });
-  }
+				return topTracks;
+			});
+		});
+	}
 
-  this.getRelatedArtists = function(artistName) {
-    return getArtistID(artistName).then(function(artistID) {
-      var artistsPromise = spotifyClient.getArtistRelatedArtists(artistID);
-      console.log('fetching related artists...');
-      console.log('artistpromise: ' + artistsPromise);
-      return artistsPromise.then(function(data) 
-      {
-        console.log(data.artists);
-        return data.artists;
-      });
-    });
-  }
+	this.getRelatedArtists = function (artistName) {
+		return getArtistID(artistName).then(function (artistID) {
+			var artistsPromise = spotifyClient.getArtistRelatedArtists(artistID);
+			//return artistsPromise.then(function (data) {
+			//	return data.artists;
+			//});
 
-  this.getArtistAlbums = function(artistName) {
-    console.log('fetching artistID...');
-    return getArtistID(artistName).then(function(artistID) {
-      console.log('fetching albums...');
-      var albumsPromise = spotifyClient.getArtistAlbums(artistID, { album_type : 'album', country : 'US', limit : 5, offset : 0 });
-      return albumsPromise.then(function(data) 
-      {
-        console.log('data:' + data);
-        return data.items;
-        // return data.albums;
-        /*
-        var topAlbums = [];
-        console.log('albums: ' + data.albums.length);
+			return artistsPromise.then(function (data) {
+				var artists = [];
+				console.log('items in dataset: ' + data.artists.length);
 
-        for (var i = 0; i < data.albums.length; i++) {
-          topAlbums.push({
-            'name': data.albums[i].name,
-            'id': data.albums[i].id,
-          });
-        }
-        return topAlbums;
-        */
+				for (var i = 0; i < data.artists.length; i++) {
+					artists.push({
+						'name': data.artists[i].name,
+						'id': data.artists[i].id,
+					});
+				}
+				return artists;
+			});
+		});
+	}
 
-        // if (data.albums.length) {
-        //   console.log('I got ' + data.albums.length + ' albums for ' + artistName +'!');
-        //   console.log('The most popular one is ' + data.albums[0].name);
-        // } else {
-        //   console.log('I didn\'t find any albums... Sorry.');
-        // }
-      });
-    });
-  }
+	this.getArtistAlbums = function (artistName) {
+		console.log('fetching artistID...');
+		return getArtistID(artistName).then(function (artistID) {
+			var albumsPromise = spotifyClient.getArtistAlbums(artistID,
+				{
+					album_type: 'album',
+					country: spotifyRegion,
+					limit: 5,
+					offset: 0
+				});
+			return albumsPromise.then(function (data) {
+				// return data.items;
+				var topAlbums = [];
+				console.log('items in dataset: ' + data.items.length);
+
+				for (var i = 0; i < data.items.length; i++) {
+					topAlbums.push({
+						'name': data.items[i].name,
+						'id': data.items[i].id,
+					});
+				}
+				return topAlbums;
+
+				// if (data.albums.length) {
+				//   console.log('I got ' + data.albums.length + ' albums for ' + artistName +'!');
+				//   console.log('The most popular one is ' + data.albums[0].name);
+				// } else {
+				//   console.log('I didn\'t find any albums... Sorry.');
+				// }
+			});
+		});
+	}
 };
